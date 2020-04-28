447b267da86d9ba27e7e96241500bfb52117d9fa
Rahul krishna <rahulkris250102@gmail.com>
2020-04-28 12:10:26 +0530

    Factor app

---
+++ app/src/main/java/com/example/factor/vModel.java
@@ -0,0 +1,11 @@
+package com.example.factor;
+
+import android.arch.lifecycle.ViewModel;
+
+public class vModel extends ViewModel {
+
+    public int score = 0, highScore, counter, rand, b1, b3;
+    public String bs1, bs2, bs3;
+    public long num = 0, b2;
+
+}
